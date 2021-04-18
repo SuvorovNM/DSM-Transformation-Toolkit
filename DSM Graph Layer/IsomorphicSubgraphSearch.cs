ad2d3f859e6389281092b29b09c47746f64c8a52
SuvorovNM <35630837+SuvorovNM@users.noreply.github.com>
2021-04-18 22:21:45 +0500

    Добавлен скелет поиска изоморфного подграфа

--- DSM Graph Layer/IsomorphicSubgraphSearch.cs
+++ DSM Graph Layer/IsomorphicSubgraphSearch.cs
@@ -1,12 +1,14 @@
 ﻿using DSM_Graph_Layer.HPGraphModel;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DSM_Graph_Layer
 {
     public class IsomorphicSubgraphFinder
     {
+        private const long nullValue = 0;
         /// <summary>
         /// Отображение i-й вершины исходного графа на вершину vCoreSource[i] графа-паттерна
         /// </summary>
@@ -65,9 +67,167 @@ namespace DSM_Graph_Layer
         {
             SourceGraph = sourceG;
             TargetGraph = targetG;
+            GeneratedAnswers = new List<(long[], long[], long[])>();
+
+            // TODO: переопределить индексы? создать словарь соответствий? !!!!!! Учеть полюса
+            vCoreSource = new long[sourceG.Vertices.Count];
+            vCoreTarget = new long[targetG.Vertices.Count];
+            vCoreSource = new long[sourceG.Vertices.Count];
+            vConnTarget = new long[targetG.Vertices.Count];
+
+            wCoreSource = new long[sourceG.Edges.Count];
+            wCoreTarget = new long[targetG.Edges.Count];
+            wSteps = new long[targetG.Edges.Count];
         }
         private HPGraph SourceGraph { get; set; }
         private HPGraph TargetGraph { get; set; }
+        public List<(long[], long[], long[])> GeneratedAnswers;
 
+        public void RecurseV() // TODO: может добавить шаг?
+        {
+            if (vCoreTarget.All(x => x != nullValue))
+            {
+                var polCorr = RecurseW();
+                if (polCorr != null)
+                {
+                    var finalCorr = AppendUnlinkedMatches(polCorr);
+                    // TODO: Уточнить о несвязных вершинах
+                    GeneratedAnswers.Add((vCoreTarget, wCoreTarget, finalCorr));
+                }
+                else
+                {
+                    RestoreVectorsV();
+                }
+            }
+            else
+            {
+                var possiblePairs = GetAllCandidatePairsV();
+                foreach(var pair in possiblePairs)
+                {
+                    if (CheckFisibilityRulesV(pair))
+                    {
+                        UpdateVectorsV(pair);
+                        RecurseV();
+                    }
+                }
+                RestoreVectorsV();
+            }
+        }
+
+        public long[] RecurseW() // TODO: Output?
+        {
+            if (wCoreTarget.All(x=>x != nullValue))
+            {
+                var incidentEdges = GroupByIncidence();
+                foreach(var pair in incidentEdges)
+                {
+                    var polCorr = RecurseP(pair); // TODO: добавить проверку на принадлежность соответствующим гиперребрам при сравнении
+                    if (!TryAppendToPoleMatching(polCorr)) 
+                    {
+                        ClearPoleVectors();
+                        break;
+                    }
+                }
+                return pCoreTarget; // А нужно ли возвращать вектор?
+            }
+
+            var possiblePairs = GetAllCandidatePairsW(); // TODO: Add feasibility rules или определить их в GetAllCandidatePairsW (наверное лучше тут)?
+            foreach (var pair in possiblePairs)
+            {
+                //TODO: возможно стоит здесь также осуществлять поиск подграфа
+                var poles = RecurseP(pair);
+                if (poles != null)
+                {
+                    UpdateVectorsW(pair);
+                    RecurseW();
+                }
+            }
+
+            RestoreVectorsW();
+            return null; // Уточнить положение return
+        }
+
+        // Похоже придется вынести в отдельный класс
+        public long[] RecurseP((Hyperedge sourceEdge, Hyperedge targetEdge) edgePair, List<long> polCorr = null) // TODO: Output?
+        {
+            if (polCorr == null)
+            {
+                polCorr = new List<long>(edgePair.targetEdge.Poles.Count);
+            }
+            if (polCorr.All(x => x != nullValue))
+                return polCorr.ToArray();
+
+            var possiblePairs = GetAllCandidatePairsP();
+            foreach (var pair in possiblePairs)
+            {
+                if (CheckFisibilityRulesP(pair))
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        public long[] AppendUnlinkedMatches(long[] polCorr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestoreVectorsV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestoreVectorsW()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVectorsV((Vertex sourceVertex, Vertex targetVertex) pair)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateVectorsW((Hyperedge sourceEdge, Hyperedge targetEdge) pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<(Vertex,Vertex)> GetAllCandidatePairsV()
+        {
+            throw new NotImplementedException();
+        }
+        public List<(Hyperedge, Hyperedge)> GetAllCandidatePairsW()
+        {
+            throw new NotImplementedException();
+        }
+        public List<(Pole, Pole)> GetAllCandidatePairsP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckFisibilityRulesV((Vertex sourceVertex, Vertex targetVertex) pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckFisibilityRulesP((Pole sourcePole, Pole targetPole) pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<(Hyperedge, Hyperedge)> GroupByIncidence()
+        {
+            // TODO: Расшифровать все гиперребра и объединить Links и Poles
+            throw new NotImplementedException();
+        }
+
+        public bool TryAppendToPoleMatching(long[] polCorr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearPoleVectors()
+        {
+
+        }
     }
 }
